c6addde8529e525f428c89862963281ce70057df
genesisazor <thegenesis3800@gmail.com>
2017-10-06 08:33:04 -0400

    alarm question
    alarm question

--- chapter2/sentence.py
+++ chapter2/sentence.py
@@ -4,13 +4,23 @@ owen = turtle.Turtle()
 
 def make_square(turt,size, num):
     for j in range(num):
+         turt.pendown()
+    for i in range(4)
          turt.forward(size)
          turt.left(90)
-         turt.forward(2*size)
+    turt.penup()
+    turt.backward(size/2)
+    turt.right(90)
+    turt.forward(size/2)
+    turt.left(90)
+    size= size *2
+
 
 wn=turtle.Screen()
+wn.bgcolor("lightgreen")
+
 owen=turtle.Turtle()
-
-
+owen.color("hotpink")
+owen.pensize("hotpink")
 
 make_square(owen,20,5)
