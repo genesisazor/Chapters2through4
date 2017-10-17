ddad48267db9477a1c9bbd7cf2824dbb05492eeb
genesisazor <thegenesis3800@gmail.com>
2017-10-17 08:26:24 -0400

    ch3question1
    question1

---
+++ Chapter 4/question 10.py
@@ -0,0 +1,13 @@
+import turtle
+wn=turtle.Screen()
+jo = turtle.Turtle()
+
+def make_star(turt)
+    for i in range(5)
+    turt.forward(100)
+    turt.right(144)
+
+    for i in range(5)
+        make_star(jo)
+        jo.forward(350)
+        jo.right(144)
