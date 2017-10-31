25ac627427635f077d188418c20061f30112fe67
genesisazor <thegenesis3800@gmail.com>
2017-10-31 10:42:23 -0400

    donewithchapter3
    donewithchapter3

--- chapter3/5d.py
+++ chapter3/5d.py
@@ -1,3 +1,6 @@
 xs = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+total = 1
 for i in xs:
-    print (i)
+
+    total = total * i
+print(total)
---
+++ chapter3/question12.py
@@ -0,0 +1,7 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+tess = turtle.Turtle()
+tess.shape("turtle")
+tess.color("blue")
+
