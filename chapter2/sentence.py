59edcfd0df15de6edd56eccee542b914c86a6b4d
genesisazor <thegenesis3800@gmail.com>
2017-10-31 09:10:26 -0400

    question1
    question1

--- chapter2/sentence.py
+++ chapter2/sentence.py
@@ -1,26 +1,14 @@
-import turtle
-wn = turtle. Screen()
-owen = turtle.Turtle()
-
-def make_square(turt,size, num):
-    for j in range(num):
-         turt.pendown()
-    for i in range(4)
-         turt.forward(size)
-         turt.left(90)
-    turt.penup()
-    turt.backward(size/2)
-    turt.right(90)
-    turt.forward(size/2)
-    turt.left(90)
-    size= size *2
+var1 = 'all'
+var2 = 'work'
+var3 = 'and'
+var4 = 'no'
+var5 = 'play'
+var6 = 'makes'
+var7 = 'jack'
+var8 = 'a'
+var9 = 'dull'
+var10= 'boy'
 
 
-wn=turtle.Screen()
-wn.bgcolor("lightgreen")
+print (var1+var2+var3+var4+var5+var6+var7+var8+var9+var10)
 
-owen=turtle.Turtle()
-owen.color("hotpink")
-owen.pensize("hotpink")
-
-make_square(owen,20,5)
